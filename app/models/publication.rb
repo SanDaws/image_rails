3c2e553359f6f3515c72a36f82dd315d78bb635a
Daws <castrogiraldosantiago@gmail.com>
2024-12-12 08:10:58 -0500

    feat: publication generation

---
+++ app/models/publication.rb
@@ -0,0 +1,3 @@
+class Publication < ApplicationRecord
+  has_one_attached :avatar
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_12_034219) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_12_130346) do
   create_schema "auth"
   create_schema "extensions"
   create_schema "graphql"
@@ -58,6 +58,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_12_034219) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "publications", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
